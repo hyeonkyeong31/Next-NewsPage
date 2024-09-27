cffeac3e848a994c6596c84b10dadf42fe122b1c
gaeng <78129949+hyeonkyeong31@users.noreply.github.com>
2024-09-27 22:31:55 +0900

    feat : add nested routes inside dynamic routes

--- components/nav-link.js
+++ components/nav-link.js
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 
 export default function NavLink({ href, children }) {
     const path = usePathname();
+
     return (
         <Link
             href="/news"
