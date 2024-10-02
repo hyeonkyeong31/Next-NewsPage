481bcd43e431d03d174f2f45f27924347c720ae2
gaeng <78129949+hyeonkyeong31@users.noreply.github.com>
2024-10-02 17:55:29 +0900

    refactor : fetchig directly from the source

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,8 +1,8 @@
 import NewsList from "@/components/news-list";
-export default async function NewsPage() {
-    const response = await fetch("http://localhost:8080/news");
-    const news = await response.json();
+import { getAllNews } from "@/lib/news";
 
+export default async function NewsPage() {
+    const news = getAllNews();
     return (
         <>
             <h1>News Page</h1>
